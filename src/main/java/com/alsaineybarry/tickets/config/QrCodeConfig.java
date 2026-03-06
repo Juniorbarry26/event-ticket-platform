0021f6bafea258335d88c872cb03530ff3710156
Juniorbarry26 <ab22316053@utg.edu.gm>
2026-03-06 23:36:12 +0000

    feat: add qr code dependency

--- src/main/java/com/alsaineybarry/tickets/config/QrCodeConfig.java
+++ src/main/java/com/alsaineybarry/tickets/config/QrCodeConfig.java
@@ -1,5 +1,6 @@
 package com.alsaineybarry.tickets.config;
 
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
